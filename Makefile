9390fb91fa7d92e39a4392db71a9e4f12cf6b9ca
Nick Cox <nickcox1008@gmail.com>
2012-05-01 20:43:27 -0600

    Converted to using the sdl backend for Protea. Converted the ITLD function to accept a 3d point.

--- Makefile
+++
@@ -1,2 +0,0 @@
-all:
-	g++ -o test testInterpolate.cpp interpolate.cpp common.cpp
--- common.cpp
+++ common.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 #define PI = 3.141592653589793;
 
+// Platform-dependent sleep routines.
+#if defined(__WINDOWS_ASIO__) || defined(__WINDOWS_DS__)
+	#include <windows.h>
+	#define SLEEP(milliseconds) Sleep((DWORD)milliseconds)
+#else // Unix variants
+	#include <unistd.h>
+	#define SLEEP(milliseconds) usleep((unsigned long)(milliseconds * 1000.0))
+#endif
+
 float dist(float Lx, float Ly, float Lz, float Rx, float Ry, float Rz) {
 	float x = Rx - Lx;
 	float y = Ry - Ly;
